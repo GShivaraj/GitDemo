2bf62a55300af769633b540b9e1ed188657b1b05
GShivaraj <sivagtesting123@gmail.com>
2022-07-20 11:08:42 +0530

    Changes for the user domin

--- src/TestNG/TestCases1.java
+++ src/TestNG/TestCases1.java
@@ -10,13 +10,33 @@ public class TestCases1 {
 	@Test
 	public void TestCases() {
 		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://www.amazon.in/");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle1());
+		System.out.println(driver.getTitle2());
+		System.out.println(driver.getTitle3());
+		System.out.println(driver.getTitle4());
+		System.out.println(driver.getTitle5());
+		System.out.println(driver.getTitle6());
+		System.out.println(driver.getTitle7());
+		System.out.println(driver.getTitle8());
+		System.out.println(driver.getTitle9());
+		System.out.println(driver.getTitle10());
+		System.out.println(driver.getTitle11());
+		System.out.println(driver.getTitle12());
+		System.out.println(driver.getTitle13());
+		System.out.println(driver.getTitle14());
+		System.out.println(driver.getTitle15());
 		
 		
 	System.out.println(driver.getTitle());
+		
+	}
+	@BeforeTest
+	public void BasicUrl{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		
 		
 	}
 
