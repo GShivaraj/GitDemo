bc68989f60344c540228677c9d32a2b9d1caaca1
GShivaraj <sivagtesting123@gmail.com>
2022-07-20 10:53:15 +0530

    first changes

--- src/TestNG/Groups.java
+++ src/TestNG/Groups.java
@@ -9,16 +9,23 @@ public class Groups {
 	
 	public void test4() {
 		System.out.println("smoke");
+		System.out.println("smoke1");
+		System.out.println("smoke2");
 	}
 
 	@Test(groups= {"smoke","functional","sanity", "ios.smoke"})
 	public void test5() {
 		System.out.println("functional");
+		System.out.println("functional1");
+		System.out.println("functional"2);
 	}
 
 	@Test(groups= {"functional", "regression","windows.functional","ios.functional"})
 	public void test6() {
 		System.out.println("regression");
+		System.out.println("regression1");
+		System.out.println("regression2");
+		System.out.println("regression3");
 	}
 
 	@Test
